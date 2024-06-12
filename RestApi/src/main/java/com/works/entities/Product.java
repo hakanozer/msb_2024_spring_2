a9a64e4d9e7952b3604deef8d1929e7c92e128ca
Hakan ÖZER <hakanozer02@gmail.com>
2024-06-12 15:54:40 +0300

    RestApi

---
+++ RestApi/src/main/java/com/works/entities/Product.java
@@ -0,0 +1,13 @@
+package com.works.entities;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private Integer price;
+
+}
